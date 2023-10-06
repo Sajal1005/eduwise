605d2a3c88609fe89386cbe195d1a45892ff6851
Sajal1005 <nandasajal.208@gmail.com>
2023-10-06 20:21:43 +0530

    added animations

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,31 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return ( 
-    <div className="h-full flex items-center justify-center">
-      {/* Component for displaying login & signup page */}
-      {children}
-    </div>
+    <div className="h-full flex items-center justify-center area">
+            <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    {/* Component for displaying login & signup page */}
+    {children}
+  </div>
    );
 }
  
